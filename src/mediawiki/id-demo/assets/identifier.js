1a9e36de7294d2464581184517b19e2f8d9db7d1
Mikhail Popov <mpopov@wikimedia.org>
2019-08-26 10:55:41 -0400

    [JS] Unequal weights bucketing + bucketing stats in demo

--- src/mediawiki/id-demo/assets/identifier.js
+++ src/mediawiki/id-demo/assets/identifier.js
@@ -13,12 +13,17 @@ class Identifier {
     id += separator + this.sequence.toString(16).padStart(4, '0');
     return id;
   }
-  inBucket(buckets) {
+  inBucket(weights) {
+    let buckets = weights.length;
     if (buckets > 1) {
-      let segment_length = 65535 / buckets;
-      for (var i = 1; i <= buckets; i++) {
-        if (this.random < (segment_length * i)) {
-          return i;
+      let segments = new Array(buckets).fill(0);
+      segments[0] = 65535 * weights[0];
+      for (var s = 1; s < buckets; s++) {
+        segments[s] = segments[s - 1] + (65535 * weights[s]);
+      }
+      for (var i = 0; i < buckets; i++) {
+        if (this.random < segments[i]) {
+          return i + 1; // number of segment aka bucket
         }
       }
       return -1;
--- src/mediawiki/id-demo/assets/init.js
+++ src/mediawiki/id-demo/assets/init.js
@@ -1,12 +1,16 @@
+const summation = (accumulator, currentValue) => accumulator + currentValue;
+
 $( function () {
 
   var id = new Identifier()
+  var stats = [1, 0, 0, 0];
+  var prob1 = [0.25, 0.25, 0.25, 0.25];
+  var prob2 = [0.4, 0.1, 0.3, 0.2];
+  var probs = prob1;
 
-	var numberOfBuckets = new OO.ui.NumberInputWidget( {
-        label: 'Number of buckets',
-        input: { value: 1 },
-        min: 1, max: 16,
-        classes: [ 'number-buckets' ]
+	var equalBucketWeights = new OO.ui.ToggleSwitchWidget( {
+      	label: 'Equal weights',
+        value: true
       } ),
       stepUpButton = new OO.ui.ButtonWidget( {
         label: 'Step',
@@ -21,12 +25,36 @@ $( function () {
 
   function updateIdentifierInfo() {
     $( "#hex-identifier" ).text(id.toHex());
-    $( "#bucket-info" ).text(id.inBucket(numberOfBuckets.getValue()))
+    $( "#bucket-info" ).text(id.inBucket(probs))
   }
-
-  numberOfBuckets.on( 'change', function ( item ) {
+  function resetBucketStats() {
+    stats = [0, 0, 0, 0]; // new Array(4).fill(0);
+  }
+  function updateBucketStatsTable() {
+    let header_row = "<th align='left'>Bucket</th>";
+    let counts_row = "<td>IDs</td>";
+    let props_row = "<td>Proportion</td>";
+    let expect_row = "<td>Expected</td>";
+    let total_ids = stats.reduce(summation);
+    for (var i = 0; i < stats.length; i++) {
+      header_row += "<th align='right'>" + (i + 1) + "</th>";
+      counts_row += "<td align='right'>" + stats[i] + "</td>";
+      props_row += "<td align='right'>" + Math.round(100 * stats[i] / total_ids) + "%</td>";
+      expect_row += "<td align='right'>" + 100 * probs[i] + "%</td>";
+    }
+    let row_sep = "</tr><tr>";
+    let stats_table = "<table><tr>" + header_row + row_sep + counts_row + row_sep + props_row + row_sep + expect_row + "</tr></table>";
+    $( "#bucketing-stats" ).html(stats_table);
+  }
+  
+  equalBucketWeights.on( 'change', function ( item ) {
+    probs = item ? prob1 : prob2;
+    resetBucketStats();
+    let b = id.inBucket(probs) - 1
+    stats[b]++;
     updateIdentifierInfo();
-  } );
+    updateBucketStatsTable();
+  } )
 
   stepUpButton.on( 'click', function ( item ) {
     id.step();
@@ -35,15 +63,19 @@ $( function () {
 
   regenIdentifierButton.on( 'click', function ( item ) {
     id = new Identifier();
+    let b = id.inBucket(probs) - 1
+    stats[b]++;
     updateIdentifierInfo();
+    updateBucketStatsTable();
   } );
 
 	// Append to the wrapper
-  $( '#bucketing-settings' ).append( numberOfBuckets.$element );
+  $( '#bucketing-settings' ).append( equalBucketWeights.$element );
 	$( '#identifier-btns' ).append(
 		stepUpButton.$element,
     regenIdentifierButton.$element
 	);
 
   updateIdentifierInfo();
+  updateBucketStatsTable();
 } );
