3de0e5fe599462209bfdd51b6eca2b0c0f238aaa
Mikhail Popov <mpopov@wikimedia.org>
2019-08-26 12:07:33 -0400

    [JS] More elegant sampling algorithms

--- src/mediawiki/id-demo/assets/sampling.js
+++ src/mediawiki/id-demo/assets/sampling.js
@@ -1,16 +1,17 @@
 class Sampling {
-  static makeSegments(weights, max_val = 65535) {
-    let segments = new Array(weights.length);
-    segments[0] = max_val * weights[0];
-    for (var s = 1; s < weights.length; s++) {
-      segments[s] = segments[s - 1] + (max_val * weights[s]);
+  static makeThresholds(weights, max_val = 65535) {
+    let segments = weights.map( function( weight ) {
+      return weight * max_val;
+    } )
+    for (var s = 1; s < segments.length; s++) {
+      segments[s] += segments[s - 1];
     }
     return segments;
   }
   static inBucket(rand, weights, max_val = 65535) {
     let buckets = weights.length;
     if (buckets > 1) {
-      let segments = Sampling.makeSegments(weights, max_val);
+      let segments = Sampling.makeThresholds(weights, max_val);
       for (var i = 0; i < buckets; i++) {
         if (rand < segments[i]) {
           return i + 1; // number of segment aka bucket
